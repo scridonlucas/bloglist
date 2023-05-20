4dc0473284300193db6ebebe5346b3367246b3cf
scridonlucas <lukas_scridon@yahoo.com>
2023-05-21 00:06:37 +0300

    fixed end error

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,13 +2,13 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blogs');
 const User = require('../models/users');
 const jwt = require('jsonwebtoken');
-
+const middleware = require('../utils/middleware');
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body;
   console.log(request.user);
   const decodedToken = jwt.verify(request.token, 'tokenpassword');
@@ -40,18 +40,48 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 });
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete(
+  '/:id',
+  middleware.userExtractor,
+  async (request, response) => {
+    const blog = await Blog.findById(request.params.id);
+    const decodedToken = jwt.verify(request.token, 'tokenpassword');
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' });
+    }
+
+    if (blog.user.toString() === decodedToken.id.toString()) {
+      await Blog.findByIdAndRemove(request.params.id);
+    }
+
+    response.status(204).end();
+  }
+);
+
+blogsRouter.put('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id);
+  const body = request.body;
   const decodedToken = jwt.verify(request.token, 'tokenpassword');
 
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' });
   }
 
-  if (blog.user.toString() === decodedToken.id.toString()) {
-    await Blog.findByIdAndRemove(request.params.id);
-  }
+  const user = await User.findById(decodedToken.id);
 
-  response.status(204).end;
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    user: user.id,
+  };
+
+  if (blog.user.toString() === decodedToken.id.toString()) {
+    const savedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {
+      new: true,
+    });
+    response.status(201).json(savedBlog);
+  }
 });
 module.exports = blogsRouter;
